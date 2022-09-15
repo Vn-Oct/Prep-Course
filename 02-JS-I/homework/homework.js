8d561dfc7d41a2093b76023528582881d9e20b13
Vn-Oct <ocete.v.n@gmail.com>
2022-09-14 23:07:28 -0300

    Final ej 2JS

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,8 +68,8 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-if (str1.lenght === str2.lenght) { return true};
-else {return false}}
+if(str1.length === str2.length) {return true}
+  else {return false};}
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
@@ -102,7 +102,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num % 3 === 0;
+  return num % 2 === 1;
 
 }
 
@@ -150,11 +150,11 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
 if (numero > 0) 
-return ["Es positivo"] ;
+{return "Es positivo"} 
 else if (numero < 0)
-return ["Es negativo"] ;
-else if (numero !== 0)
-return false
+{return "Es negativo"}
+else (numero === 0)
+{return false}
 ;}
 
 function agregarSimboloExclamacion(str) {
@@ -175,27 +175,28 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return obtenerSaludo=('Hola, yo soy'+' '+ nombre);
+  return 'Hola'+' '+ nombre +'!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return obtenerAreaRectangulo=(alto * 2) + (ancho * 2);
+  return obtenerAreaRectangulo=(alto * ancho);
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  return (lado * 2) ;
+  return (lado * 4) ;
 }
 
 function areaDelTriangulo(base, altura){
 
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
- return (base * altura) / 2 ;
+ var resultado=((base * altura) / 2)
+ return resultado ;
 }
 
 
@@ -214,6 +215,10 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+
+  if (letra.lenght < 1) {return 'Dato incorrecto'}
+    else if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {return 'Es vocal'}
+  else {return 'Dato incorrecto'};
 }
 
 
